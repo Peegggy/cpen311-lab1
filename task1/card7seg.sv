827d51c9c66344c8cd980c85dc58011083926adb
peegggy <72671394+Peegggy@users.noreply.github.com>
2021-05-16 17:21:23 -0700

    first commit: card7seg code works on DE1 SOC

--- task1/card7seg.sv
+++ task1/card7seg.sv
@@ -1,6 +1,36 @@
 module card7seg(input logic [3:0] SW, output logic [6:0] HEX0);
 		
-   // your code goes here
+  always_comb begin
+   HEX0 = 7'bx;
+   if(SW == 4'b0000)//blank
+      HEX0 = 7'b1111111;
+   if(SW == 4'b0001)//ace
+      HEX0 = 7'b0001000; //display A
+   if(SW == 4'b0010)//2
+      HEX0 = 7'b0100100;
+   if(SW == 4'b0011)//3
+      HEX0 = 7'b0110000;
+   if(SW == 4'b0100)//4
+      HEX0 = 7'b0011001;
+   if(SW == 4'b0101)//5
+      HEX0 = 7'b0010010;
+   if(SW == 4'b0110)//6
+      HEX0 = 7'b0000010;
+   if(SW == 4'b0111)//7
+      HEX0 = 7'b1111000;
+   if(SW == 4'b1000)//8
+      HEX0 = 7'b0;
+   if(SW == 4'b1001)//9
+      HEX0 = 7'b0010000;
+   if(SW == 4'b1010)//0
+      HEX0 = 7'b1000000;
+   if(SW == 4'b1011)//jack
+      HEX0 = 7'b1100001; //display j
+   if(SW == 4'b1100)//queen
+      HEX0 = 7'b0011000; //display q
+   if(SW == 4'b1101)//king
+      HEX0 = 7'b0001001;  //display k
+  end
 	
 endmodule
 
--- task1/tb_card7seg.sv
+++ task1/tb_card7seg.sv
@@ -1,8 +1,95 @@
 module tb_card7seg();
 
-// Your testbench goes here. Make sure your tests exercise the entire design
-// in the .sv file.  Note that in our tests the simulator will exit after
-// 10,000 ticks (equivalent to "initial #10000 $finish();").
-						
+  logic [3:0] SW;
+  logic [6:0] HEX0;
+
+  card7seg dut(.*);
+
+/* if the code fails some test cases then the error message "Error: Assertion error"
+   will display in the transcript window, else the code works and passes all cases.*/
+
+  initial begin
+     SW <= 4'd0; //blank
+     #10;
+     assert(HEX0 === 7'b1111111); //check to see if the LED output is nothing
+     #10;
+     
+     SW <= 4'd1; //ace
+     #10;
+     assert(HEX0 === 7'b0001000); //output is A?
+     #10;
+
+     SW <= 4'd2; //2
+     #10;
+     assert(HEX0 === 7'b0100100); //output is 2?
+     #10;
+
+     SW <= 4'd3; //3
+     #10;
+     assert(HEX0 === 7'b0110000); //output is 3?
+     #10;
+
+     SW <= 4'd4; //4
+     #10;
+     assert(HEX0 === 7'b0011001); //output is 4?
+     #10;
+
+     SW <= 4'd5; //5
+     #10;
+     assert(HEX0 === 7'b0010010); //output is 5?
+     #10;
+
+     SW <= 4'd6; //6
+     #10;
+     assert(HEX0 === 7'b0000010); //output is 6?
+     #10;
+
+     SW <= 4'd7; //7
+     #10;
+     assert(HEX0 === 7'b1111000); //output is 7?
+     #10;
+
+     SW <= 4'd8; //8
+     #10;
+     assert(HEX0 === 7'b0); //output is 8?
+     #10;
+
+     SW <= 4'd9; //9
+     #10;
+     assert(HEX0 === 7'b0010000); //output is 9?
+     #10;
+
+     SW <= 4'd10; //10
+     #10;
+     assert(HEX0 === 7'b1000000); //output is 0?
+     #10;
+
+     SW <= 4'd11; //jack
+     #10;
+     assert(HEX0 === 7'b1100001); //output is j?
+     #10;
+
+     SW <= 4'd12; //queen
+     #10;
+     assert(HEX0 === 7'b0011000); //output is q?
+     #10;
+
+     SW <= 4'd13; //king
+     #10;
+     assert(HEX0 === 7'b0001001); //output is k?
+     #10;
+     
+     SW <= 4'd14; //14
+     #10;
+     assert(HEX0 === 7'b1); //no such card, output should be blank
+     #10;
+
+     SW <= 4'd15; //15
+     #10;
+     assert(HEX0 === 7'b1); //no such card, output should be blank
+     #10;
+
+     $stop;
+   end
 endmodule
 
